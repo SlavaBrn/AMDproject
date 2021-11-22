0ae745529413659864a02f6e8cb71fa316e3fdf4
slavon <yr.broner@gmail.com>
2021-11-21 21:36:59 -0600

    const change

--- src/main/java/Pages/Const.java
+++ src/main/java/Pages/Const.java
@@ -2,4 +2,5 @@ package Pages;
 
 public class Const {
   public static final String LOGIN_URL = "https://amdtp-mock-qa.phoenixfms.ca/";
+//  public static final String LOGIN_URL = "https://amdtp-mock-qa.phoenixfms.ca/";
 }
